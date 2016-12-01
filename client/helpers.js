105f598d867920c0218f4dbf6c533e372101732a
Sergey Piterman <sergeypiterman@gmail.com>
2016-11-30 17:48:42 -0800

    [refactor] Move generate endpoints to helpers

--- client/helpers.js
+++ client/helpers.js
@@ -14,3 +14,32 @@ function isValidPosition(x1, y1){
   }
   return true;
 }
+
+function generateEndpoints(start, end){
+
+  var startNode = canvas.getLayer(start);
+  var endNode = canvas.getLayer(end);
+
+  var x1, x2, y1, y2;
+  var ratio = ((end.y - start.y)/(end.x - start.x))
+  var theta = Math.atan(ratio)
+  if(end.x <= start.x) {
+    x1 = start.x - Math.cos(theta) * neuronRadius;
+    y1 = start.y - Math.sin(theta) * neuronRadius;
+    x2 = end.x + Math.cos(theta) * neuronRadius;
+    y2 = end.y + Math.sin(theta) * neuronRadius;
+  } else {
+    x1 = start.x + Math.cos(theta) * neuronRadius;
+    y1 = start.y + Math.sin(theta) * neuronRadius;
+    x2 = end.x - Math.cos(theta) * neuronRadius;
+    y2 = end.y - Math.sin(theta) * neuronRadius;
+  }
+
+  return {
+    x1: x1,
+    x2: x2,
+    y1: y1,
+    y2: y2,
+    name: (start + '-' + end)
+  };
+}
